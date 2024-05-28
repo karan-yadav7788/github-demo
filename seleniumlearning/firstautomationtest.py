e345974fc32fcfef802dc9e6dcfa99e395cb1499
karan yadav <yadav.karan778899@gmail.com>
2024-05-28 17:01:37 +0530

    thirdcommit

--- seleniumlearning/firstautomationtest.py
+++ seleniumlearning/firstautomationtest.py
@@ -1,11 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-import time
-driver=webdriver.Chrome()
-driver.get("")
-driver.maximize_window()
-time.sleep(10)
-driver.find_element(By.XPATH, "").click()
-bb = Select(driver.find_element((By.XPATH, "")))
-bb.select_by_visible_
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end = " ")
+    print("")
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*", end=" ")
+    print("")
+
